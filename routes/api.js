6d515829d15caa0a1221b973a8fcb31c6004ebb2
delm83 <delm83@googlemail.com>
2024-01-27 10:09:57 +0000

    added schema

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,35 @@ module.exports = function (app) {
   const mongoose = require('mongoose');
   mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true});
 
+  let Schema = mongoose.Schema;
+
+  let issueSchema = new Schema({
+    title: {
+    type: String,
+    required: true,
+   },
+    text: {
+    type: String,
+    required: true
+   },
+    created_by: {
+    type: String,
+    required: true
+    },
+    assigned_to: {
+    type: String,
+    defualt: ''
+    },
+    status_text: {
+    type: String,
+    default: ''},
+    created_on: String,
+    updated_on: String,
+    open: {
+    type: Boolean,
+    default: false}
+ });
+
   app.route('/api/issues/:project')
   
     .get(function (req, res){
