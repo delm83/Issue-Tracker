3b7efd6c6777bfee19852e507ae858101076cb24
delm83 <delm83@googlemail.com>
2024-01-28 09:30:56 +0000

    posted first issue to db

--- routes/api.js
+++ routes/api.js
@@ -27,11 +27,11 @@ module.exports = function (app) {
     status_text: {
     type: String,
     default: ''},
-    created_on: String,
-    updated_on: String,
+    created_on: Date,
+    updated_on: Date,
     open: {
     type: Boolean,
-    default: false}
+    default: true}
  });
 
  let Issue = mongoose.model('Issue', issueSchema);
@@ -43,9 +43,30 @@ module.exports = function (app) {
       
     })
     
-    .post(function (req, res){
+    .post(async (req, res)=>{
+      try{
       let project = req.params.project;
-      
+      let title = req.body.issue_title;
+      let text = req.body.issue_text;
+      let created_by = req.body.created_by;
+      let assigned_to = req.body.assigned_to;
+      let status_text = req.body.status_text;
+
+      let inputIssue = new Issue({
+        title: title,
+        text: text,
+        created_by: created_by,
+        assigned_to: assigned_to,
+        status_text: status_text,
+        created_on: new Date(),
+        updated_on: new Date()
+    });
+
+    await inputIssue.save();
+
+    res.json({_id: inputIssue.id, title: inputIssue.title, text: inputIssue.text, created_by: inputIssue.created_by, assigned_to: inputIssue.assigned_to, status_text: inputIssue.status_text, created_on: inputIssue.created_on, updated_on: inputIssue.updated_on, open: inputIssue.open})
+
+    }catch(err){return res.json({error: err})}
     })
     
     .put(function (req, res){
