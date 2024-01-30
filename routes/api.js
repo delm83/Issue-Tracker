e08772afae900bd4504dca0b5c73d8a4bc7cd358
delm83 <delm83@googlemail.com>
2024-01-30 18:19:19 +0000

    deleted duplicate post function

--- routes/api.js
+++ routes/api.js
@@ -81,38 +81,7 @@ module.exports = function (app) {
 
     }catch(err){return res.json({error: err})}
     })
-    
-  .post(async (req, res)=>{
-    try{
-    let project = req.params.project;
-    let Issue = mongoose.model(project, issueSchema);
-    let issue_title = req.body.issue_title;
-    let issue_text = req.body.issue_text;
-    let created_by = req.body.created_by;
-    let assigned_to = req.body.assigned_to;
-    let status_text = req.body.status_text;
-
-    if(!issue_title || !issue_text || !created_by) {
-      return res.json({ error: 'required field(s) missing' });
-    }
-
-    let inputIssue = new Issue({
-      issue_title: issue_title,
-      issue_text: issue_text,
-      created_by: created_by,
-      assigned_to: assigned_to,
-      status_text: status_text,
-      created_on: new Date(),
-      updated_on: new Date()
-  });
-
-  await inputIssue.save();
-
-  res.json({assigned_to: inputIssue.assigned_to, status_text: inputIssue.status_text, open: inputIssue.open, _id: inputIssue.id, issue_title: inputIssue.issue_title, issue_text: inputIssue.issue_text, created_by: inputIssue.created_by, created_on: inputIssue.created_on, updated_on: inputIssue.updated_on})
-
-  }catch(err){return res.json({error: err})}
-  })
-    
+       
     .put(function (req, res){
       let project = req.params.project;
     })
