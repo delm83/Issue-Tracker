d17bd6384d144a1ef84ec3f20a57d0af707a5424
delm83 <delm83@googlemail.com>
2024-02-02 14:46:02 +0000

    beginning functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,27 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-  
+    this.timeout(5000);
+    test('Test POST /every field', function (done) {
+        chai
+          .request(server)
+          .keepOpen()
+          .post('/api/issues/apitest')
+          .send({
+            issue_title: 'First Test',
+            issue_text: 'posting every field',
+            created_by: 'Derek',
+            assigned_to: 'Jim',
+            status_text: 'ongoing'
+          })
+          .end(function (err, res) {
+            assert.equal(res.status, 200);
+            assert.equal('First Test', res.body.issue_title);
+            assert.equal('posting every field', res.body.issue_text);
+            assert.equal('Derek', res.body.created_by);
+            assert.equal('Jim', res.body.assigned_to);
+            assert.equal('ongoing', res.body.status_text);
+            done();
+          });
+      });
 });
